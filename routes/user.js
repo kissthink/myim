47777d19bce0caa13b48d43912d21663bd111e5a
gaoc <sfme@qq.com>
2015-08-10 20:03:51 +0800

    加入login处理代码

--- routes/user.js
+++ routes/user.js
@@ -6,73 +6,88 @@
  */
 
 exports.main = function(packet, context) {
-	//console.log(packet);
-	var topic = packet.topic;
-	var obj = JSON.parse(packet.payload);
-	//如果是自己的clientId就忽略
-	//if(context.config.mqtt.clientId == obj.clientId) return;
+    //console.log(packet);
+    var topic = packet.topic;
+    var obj = JSON.parse(packet.payload);
+    //如果是自己的clientId就忽略
+    //if(context.config.mqtt.clientId == obj.clientId) return;
 
-	if('' === obj.cmd) return;
-	if('signup' == obj.cmd) {
-		signUp(topic, obj, context);
-		return;
-	}
-	if('login' == obj.cmd){
-		logIn(topic, obj, context);
-	}
-	if('logout' == obj.cmd){
-		logOut(topic, obj, context);
-	}
+    if ('' === obj.cmd) return;
+    if ('signup' == obj.cmd) {
+        signUp(topic, obj, context);
+        return;
+    }
+    if ('login' == obj.cmd) {
+        logIn(topic, obj, context);
+    }
+    if ('logout' == obj.cmd) {
+        logOut(topic, obj, context);
+    }
 };
 
 /*
  * 用户注册im系统
  */
-function signUp(topic, obj, context){
-	var reObj = {};
-	reObj.cmd = 're_signup';
-	reObj.clientid = context.config.mqtt.clientId;
-	if('' === obj.usr){
-		reObj.code = 2001;
-		context.client.publish(topic, JSON.stringify(reObj));
-		return;
-	}
-	if(context.config.sysuser.usr == obj.usr){
-		reObj.code = 2002;
-		context.client.publish(topic, JSON.stringify(reObj));
-		return;
-	}
+function signUp(topic, obj, context) {
+    var reObj = {};
+    reObj.cmd = 're_signup';
+    reObj.clientid = context.config.mqtt.clientId;
+    if ('' === obj.usr) {
+        reObj.code = 2001;
+        context.client.publish(topic, JSON.stringify(reObj));
+        return;
+    }
+    if (context.config.sysuser.usr == obj.usr) {
+        reObj.code = 2002;
+        context.client.publish(topic, JSON.stringify(reObj));
+        return;
+    }
 
-	var User = context.store.getUserModel();
-	User.find({'usr': obj.usr}, 'usr pwd', function(err, usr){
-		if(err) { //查询错误
-			reObj.code = 2005;
-			console.log(err);
-		}
-		if(0 == usr.length){ //不存在,则创建
-			var user = new User();
-			user.usr = obj.usr;
-			user.pwd = obj.pwd;
-			user.save();
-			reObj.code = 2000;
-		}
-		if(0 < usr.length){ //已存在
-			reObj.code = 2002;
-		}
-		context.client.publish(topic, JSON.stringify(reObj));
-	});
+    var User = context.store.getUserModel();
+	User.find( {'usr': obj.usr}, 'usr pwd', function(err, usr) {
+        if (err) { //查询错误
+            reObj.code = 2005;
+            console.log(err);
+        }
+        if (0 == usr.length) { //不存在,则创建
+            var user = new User();
+            user.usr = obj.usr;
+            user.pwd = obj.pwd;
+            user.save();
+            reObj.code = 2000;
+        }
+        if (0 < usr.length) { //已存在
+            reObj.code = 2002;
+        }
+        context.client.publish(topic, JSON.stringify(reObj));
+    });
 };
 
 /*
  * 用户登录im系统
  */
-function logIn(topic, obj, context){
-	//TODO:
+function logIn(topic, obj, context) {
+	var reObj = {};
+    var User = context.store.getUserModel();
+	User.find( {'usr': obj.usr}, 'usr pwd', function(err, usr) {
+        if (err) { //查询错误
+            reObj.code = 2005;
+            console.log(err);
+        }
+        if (0 < usr.length) { //已存在
+            reObj.code = 2002;
+			reObj.room = 'yncoder';
+        }
+		if(0 == usr.length){ //不存在
+			reObj.code = 2001;
+		}
+        context.client.publish(topic, JSON.stringify(reObj));
+    });
 };
 
 /*
  * 用户登出im系统
  */
-function logOut(topic, obj, context){
-	//TODO:
+function logOut(topic, obj, context) {
+    //TODO:
 };
