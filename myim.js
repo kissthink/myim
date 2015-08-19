a83683fdc00b5caeaf9d59a005fa337247d7b261
gaoc <sfme@qq.com>
2015-08-19 15:18:18 +0800

    修正事件处理函数无法删除的bug

--- myim.js
+++ myim.js
@@ -11,6 +11,7 @@ var path = require('path');
 var EventEmitter = require('events').EventEmitter;
 var event = new EventEmitter();
 var mongoose = require('mongoose');
+var crypto = require('crypto');
 var Store = require('./store');
 var route = require('./routes');
 
@@ -43,9 +44,7 @@ client.on('connect', function(packet) {
 		});
 
 		//先移除事件处理,再注册指定的topic name对应的处理时间函数
-		event.removeListener(topic.name, function(err){
-			if(err) return;
-		});
+		event.removeAllListeners(topic.name);
 		event.on(topic.name, route[topic.name]);
 		console.log('event on ' + topic.name);
 	});
@@ -54,49 +53,22 @@ client.on('connect', function(packet) {
 //重连时设置
 client.on('reconnect', function(){
 	console.log('reconnect......');
-	var topics = config.topics;
-	topics.forEach(function(topic){
-		//先取消订阅,再订阅config中定义的topic
-		client.unsubscribe(topic.topic);
-		client.subscribe(topic.topic, {qos: topic.qos}, function(err, granted){
-			if(!err) console.log(topic.topic + ' subscribe sucessed.');
-		});
-
-		//先移除事件处理,再注册指定的topic name对应的处理时间函数
-		event.removeListener(topic.name, function(err){
-			if(err) return;
-		});
-		event.on(topic.name, route[topic.name]);
-		console.log('event on ' + topic.name);
-	});
 });
 
 //关闭时清理
 client.on('close', function(){
-	var topics = config.topics;
-	topics.forEach(function(topic){
-		//取消订阅
-		client.unsubscribe(topic.topic, function(err){
-			if(err) return;
-		});
-
-		//删除注册的事件
-		event.removeListener(topic.name, function(err){
-			if(err) return;
-		});
-	});
+	console.log('disconnection.');
 });
 
 //离线时处理 
 client.on('offline', function(){
-	console.log('broker offline.');
+	console.log('myim offline.');
 });
 
 //错误处理
 client.on('error', function(err){
 	if(err){
-		//console.log(err);
-		client.emit('reconnect');
+		console.log(err);
 	}
 });
 
